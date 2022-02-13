1a4341920294b2eb1fe211252a29711065d3bcd3
brenhamp <bpozzi@trinity.edu>
2022-02-12 21:23:57 -0600

    fixed server connection, changed post_url to post_body

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,17 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+// create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
+<<<<<<< Updated upstream
       port: 3001
+=======
+      port: 3306
+>>>>>>> Stashed changes
     });
 
 module.exports = sequelize;
